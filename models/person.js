7badfac3b113bb5c0e04f4f1a2c508b544f322ab
nigelpoh <58196088+nigelpoh@users.noreply.github.com>
2024-01-18 18:47:20 +0200

    bugfix: fixed phone number regex

--- models/person.js
+++ models/person.js
@@ -20,7 +20,7 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
     validate: {
       validator: function(v) {
-        return /(\d{2}|\d{3})-\d{1,}/.test(v)
+        return /^(\d{2}|\d{3})-\d{1,}$/.test(v)
       },
       message: props => `${props.value} is not a valid phone number!`
     },
